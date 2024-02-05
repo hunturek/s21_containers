710767aed1960a5efc1a4ab9f9f9d33e7be48a65
Denis <shahovdenis@bk.ru>
2024-02-05 17:10:23 +0300

    bitree balance

--- src/bitree/bitree.h
+++ src/bitree/bitree.h
@@ -3,205 +3,175 @@
 #define RED 1
 #define BLACK 0
 
+
 class bitree {
 private:
   typedef struct node {
     int value = 0;
-    node* parrent = nullptr;
-    node* left = nullptr;
-    node* right = nullptr;
-    int color = 0; //0 - black, 1 - red
-  }node;
+    node *parrent;
+    node *left;
+    node *right;
+    int color;
+  } node;
 
   node *root;
-  node *iterator;
 
-  int balance(node*);
-  int lr_check(node*);
-  int rr_check(node*);
-  int rc_check(node*);
-  void PreOrder(node*);
-  int lr(node*); //left rotate
-  int rr(node*); //right rotate
-  int rc(node*); //recolor
+  int balance(node *);
+  int lr(node *); // left rotate
+  int rr(node *); // right rotate
+  int rc(node *); // recolor
 
-  int add_node(const int value);
-  int first_root(const int value);
-  
 public:
-  bitree(){ root = nullptr;
-            iterator = nullptr; }
-  ~bitree(){ }
+  bitree() {
+    root = nullptr;
+  }
+  ~bitree() {}
   int set(const int value);
-  int set(const int* value, size_t size);
+  int set(const int *value, size_t size);
   void show();
 };
 
-int bitree::set(const int value){
-  int exit = 0;
-  int null_flag = 0;
-  if(root == nullptr){
-    iterator = new node;
-    first_root(value);
-    return exit;
+int bitree::set(const int value) {
+  node *current, *parrent, *new_node;
+  current = root;
+  parrent = nullptr;
+  while (current != nullptr) {
+    if (value == current->value)
+      return 1;
+    parrent = current;
+    current = value < current->value ? current->left : current->right;
+  }
+
+  new_node = new node;
+  new_node->value = value;
+  new_node->parrent = parrent;
+  new_node->left = nullptr;
+  new_node->right = nullptr;
+  new_node->color = RED;
+
+  if (parrent) {
+    if (value < parrent->value)
+      parrent->left = new_node;
+    else
+      parrent->right = new_node;
   } else {
-    iterator = root;
+    root = new_node;
   }
-  while(!null_flag && exit == 0){
-    if(value < iterator->value)
-      if(iterator->left != nullptr) {
-        iterator = iterator->left;
-      } else {
-        null_flag = 1;
-        iterator->left = new node;
-        iterator->left->parrent = iterator;
-        iterator = iterator->left;
-        add_node(value);
-      }
-    else if(value > iterator->value)
-      if(iterator->right != nullptr) {
-        iterator = iterator->right;
-      } else {
-        null_flag = 1;
-        iterator->right = new node;
-        iterator->right->parrent = iterator;
-        iterator = iterator->right;
-        add_node(value);
-      }
-    else if(value == iterator->value)
-      exit = 1;
-  }
-  iterator = root;
-  balance(root);
-  if(exit == 1)
-    std::cout << "exit = 1" << std::endl;
-  return exit;
+
+  balance(new_node);
+  return 0;
 }
 
-int bitree::set(const int* value, size_t size){
+int bitree::set(const int *value, size_t size) {
   int exit = 0;
-  for(size_t i = 0; i < size && exit == 0; i++)
+  for (size_t i = 0; i < size && exit == 0; i++)
     exit = set(value[i]);
   return exit;
 }
 
-int bitree::first_root(const int value){
-  iterator->value = value;
-  iterator->parrent = nullptr;
-  iterator->left = nullptr;
-  iterator->right = nullptr;
-  iterator->color = BLACK;
-  root = iterator;
-  return 0;
-}
-
-int bitree::add_node(const int value){
-  iterator->value = value;
-  iterator->left = nullptr;
-  iterator->right = nullptr;
-  iterator->color = RED;
-  return 0;
-}
-
-void bitree::show(){
-  iterator = root;
+void bitree::show() {
+  node *iterator = root;
   char code = 0;
-  while(code != 'q'){
+  while (code != 'q') {
     std::cout << iterator->value << "(" << iterator->color << ")";
     std::cout << std::endl;
     std::cin >> code;
-    if(code == 'l' && iterator->left != nullptr)
+    if (code == 'l' && iterator->left != nullptr)
       iterator = iterator->left;
-    if(code == 'r' && iterator->right != nullptr)
+    if (code == 'r' && iterator->right != nullptr)
       iterator = iterator->right;
-    if(code == 'u' && iterator->parrent != nullptr)
+    if (code == 'u' && iterator->parrent != nullptr)
       iterator = iterator->parrent;
   }
 }
 
-int bitree::balance(node *nd){
-  if(lr_check(nd)){
-    lr(nd);
-    balance(root);
-  }
-  if(rr_check(nd)){
-    rr(nd);
-    balance(root);
-  }
-  if(rc_check(nd)){
-    rc(nd);
-    balance(root);
-  }
-  if(nd->left != nullptr)
-    balance(nd->left);
-  if(nd->right != nullptr)
-    balance(nd->right);
+int bitree::balance(node *nd) {
+  node *father = nd->parrent;
+  node *grandfather;
+  if(father)
+    grandfather = father->parrent;
+    while (nd != root && nd->parrent->color == RED) {
+        if (father == grandfather->left) {
+            node *uncle = grandfather->right;
+            if (uncle && uncle->color == RED) {
+                rc(grandfather);
+                nd = grandfather;
+            } else {
+                if (nd == father->right) {
+                    nd = father;
+                    lr(nd);
+                }
+                father->color = BLACK;
+                grandfather->color = RED;
+                rr(grandfather);
+            }
+        } else {
+            node *uncle = grandfather->left;
+            if (uncle && uncle->color == RED) {
+                rc(grandfather);
+                nd = grandfather;
+            } else {
+                if (nd == father->left) {
+                    nd = father;
+                    rr(nd);
+                }
+                nd->parrent->color = BLACK;
+                grandfather->color = RED;
+                lr(grandfather);
+            }
+        }
+    }
+    root->color = BLACK;  
+    return 0;
+}
+
+int bitree::rc(node *nd) {
+  nd->left->color = BLACK;
+  nd->right->color = BLACK;
+  nd->color = RED;
   return 0;
 }
 
-int bitree::rc_check(node *nd){
-  if(nd->left != nullptr && nd->right != nullptr)
-    if(nd->left->color == 1 && nd->right->color == 1)
-      return 1;
+int bitree::lr(node *nd) {
+  node *pNode = nd;
+  node *cNode = nd->right;
+  pNode->right = cNode->left;
+  if (cNode->left != nullptr)
+    cNode->left->parrent = pNode;
+  if (cNode != nullptr)
+    cNode->parrent = pNode->parrent;
+  if (pNode->parrent) {
+    if (pNode == pNode->parrent->left)
+      pNode->parrent->left = cNode;
     else
-      return 0;
-}
-
-int bitree::lr_check(node* nd){
-  if(nd->left != nullptr && nd->right != nullptr)
-    if(nd->left->color == 0 && nd->right->color == 1)
-      return 1;
-    else
-      return 0;
-}
-
-int bitree::rr_check(node* nd){
-  if(nd->left != nullptr)
-    if(nd->left->left != nullptr)
-      if(nd->left->color == 1 && nd->left->left->color == 1)
-        return 1;
-      else
-        return 0;
-}
-
-int bitree::rc(node *nd){
-  nd->left->color = 0;
-  nd->right->color = 0;
-  nd->color = 1;
-  root->color = 0;
-  return 0;
-}
-
-int bitree::lr(node *nd){
-  node* parrentNode = nd;
-  node* childNode = nd->right;
-  childNode->parrent = parrentNode->parrent;
-  parrentNode->parrent = childNode;
-  parrentNode->right = childNode->left;
-  parrentNode->right->parrent=parrentNode;
-  childNode->left = parrentNode;
-  int tmp = parrentNode->color;
-  parrentNode->color = childNode->color;
-  childNode->color = tmp;
-  if(root == parrentNode)
-    root = childNode;
-  return 0;
-}
-
-int bitree::rr(node *nd){
-  node* parrentNode = nd;
-  node* childNode = nd->left;
-  childNode->parrent = parrentNode->parrent;
-  parrentNode->parrent = childNode;
-  if(childNode->right != nullptr){
-    parrentNode->left = childNode->right;
-    parrentNode->left->parrent=parrentNode;
+      pNode->parrent->right = cNode;
+  } else {
+    root = cNode;
   }
-  childNode->right = parrentNode;
-  int tmp = parrentNode->color;
-  parrentNode->color = childNode->color;
-  childNode->color = tmp;
-  if(root == parrentNode)
-    root = childNode;
+  cNode->left = pNode;
+  if (pNode != nullptr)
+    pNode->parrent = cNode;
+  return 0;
+}
+
+int bitree::rr(node *nd) {
+  node *pNode = nd;
+  node *cNode = nd->left;
+  pNode->left = cNode->right;
+  if (cNode->right != nullptr)
+    cNode->right->parrent = pNode;
+  if (cNode != nullptr)
+    cNode->parrent = pNode->parrent;
+  if (pNode->parrent) {
+    if (pNode == pNode->parrent->right)
+      pNode->parrent->right = cNode;
+    else
+      pNode->parrent->left = cNode;
+  } else {
+    root = cNode;
+  }
+  cNode->right = pNode;
+  if (pNode != nullptr)
+    pNode->parrent = cNode;
   return 0;
 }
