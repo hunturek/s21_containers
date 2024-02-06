d6d061e702b2ef29b6488d67368e09b1d6bcb714
Hunt Urek <hunturek@oa-n6.msk.21-school.ru>
2024-02-06 17:15:30 +0300

    del

--- src/bitree/bitree.h
+++ src/bitree/bitree.h
@@ -7,7 +7,7 @@ template <typename T> class bitree {
 private:
   typedef struct node {
     T value;
-    node *parrent;
+    node *parent;
     node *left;
     node *right;
     int color;
@@ -15,7 +15,9 @@ private:
 
   node *root;
 
-  int balance(node *);
+  int add_balance(node *);
+  int del_balance(node *);
+  node *find(const T);
   int lr(node *); // left rotate
   int rr(node *); // right rotate
   int rc(node *); // recolor
@@ -23,46 +25,93 @@ private:
 public:
   bitree() { root = nullptr; }
   ~bitree() {}
-  int set(const T value);
-  int set(const T *values, size_t size);
+  int add(const T);
+  int add(const T *, size_t);
+  int del(const T);
+  int del(const T *, size_t);
   void show();
 };
 
-template <typename T> int bitree<T>::set(const T value) {
-  node *current, *parrent, *new_node;
+template <typename T> int bitree<T>::add(const T value) {
+  node *current, *parent, *new_node;
   current = root;
-  parrent = nullptr;
+  parent = nullptr;
   while (current != nullptr) {
     if (value == current->value)
       return 1;
-    parrent = current;
+    parent = current;
     current = value < current->value ? current->left : current->right;
   }
 
   new_node = new node;
   new_node->value = value;
-  new_node->parrent = parrent;
+  new_node->parent = parent;
   new_node->left = nullptr;
   new_node->right = nullptr;
   new_node->color = RED;
 
-  if (parrent) {
-    if (value < parrent->value)
-      parrent->left = new_node;
+  if (parent) {
+    if (value < parent->value)
+      parent->left = new_node;
     else
-      parrent->right = new_node;
+      parent->right = new_node;
   } else {
     root = new_node;
   }
 
-  balance(new_node);
+  add_balance(new_node);
   return 0;
 }
 
-template <typename T> int bitree<T>::set(const T *values, size_t size) {
+template <typename T> int bitree<T>::add(const T *values, size_t size) {
   int exit = 0;
   for (size_t i = 0; i < size && exit == 0; i++)
-    exit = set(values[i]);
+    exit = add(values[i]);
+  return exit;
+}
+
+template <typename T> typename bitree<T>::node* bitree<T>::find(const T value) {
+  node *iterator = root;
+  while (iterator != nullptr)
+    if (value == iterator->value)
+      return iterator;
+    else
+      iterator = value < iterator->value ? iterator->left : iterator->right;
+  return 0;
+}
+
+template <typename T> int bitree<T>::del(const T value) {
+	node *x, *y, *z = find(value);
+    if (!z || z == nullptr) return 1;
+    if (z->left == nullptr || z->right == nullptr) {
+        y = z;
+    } else {
+        y = z->right;
+        while (y->left != nullptr) y = y->left;
+    }
+    if (y->left != nullptr)
+        x = y->left;
+    else
+        x = y->right;
+    x->parent = y->parent;
+    if (y->parent)
+        if (y == y->parent->left)
+            y->parent->left = x;
+        else
+            y->parent->right = x;
+    else
+        root = x;
+    if (y != z) z->value = y->value;
+    if (y->color == BLACK)
+        del_balance(x);
+    free(y);
+	return 0;
+}
+
+template <typename T> int bitree<T>::del(const T *values, size_t size) {
+  int exit = 0;
+  for (size_t i = 0; i < size && exit == 0; i++)
+    exit = del(values[i]);
   return exit;
 }
 
@@ -77,45 +126,101 @@ template <typename T> void bitree<T>::show() {
       iterator = iterator->left;
     if (code == 'r' && iterator->right != nullptr)
       iterator = iterator->right;
-    if (code == 'u' && iterator->parrent != nullptr)
-      iterator = iterator->parrent;
+    if (code == 'u' && iterator->parent != nullptr)
+      iterator = iterator->parent;
   }
 }
 
-template <typename T> int bitree<T>::balance(node *x) {
-  while (x != root && x->parrent->color == RED) {
-    if (x->parrent == x->parrent->parrent->left) {
-      node *y = x->parrent->parrent->right;
-      if (y && y->color == RED) {
-        rc(x->parrent->parrent);
-        x = x->parrent->parrent;
+template <typename T> int bitree<T>::add_balance(node *nd) {
+  while (nd != root && nd->parent->color == RED) {
+    if (nd->parent == nd->parent->parent->left) {
+      node *uncle = nd->parent->parent->right;
+      if (uncle && uncle->color == RED) {
+        rc(nd->parent->parent);
+        nd = nd->parent->parent;
       } else {
-        if (x == x->parrent->right) {
-          x = x->parrent;
-          lr(x);
+        if (nd == nd->parent->right) {
+          nd = nd->parent;
+          lr(nd);
         }
-        x->parrent->color = BLACK;
-        x->parrent->parrent->color = RED;
-        rr(x->parrent->parrent);
+        nd->parent->color = BLACK;
+        nd->parent->parent->color = RED;
+        rr(nd->parent->parent);
       }
     } else {
-      node *y = x->parrent->parrent->left;
-      if (y && y->color == RED) {
-        rc(x->parrent->parrent);
-        x = x->parrent->parrent;
+      node *uncle = nd->parent->parent->left;
+      if (uncle && uncle->color == RED) {
+        rc(nd->parent->parent);
+        nd = nd->parent->parent;
       } else {
-        if (x == x->parrent->left) {
-          x = x->parrent;
-          rr(x);
+        if (nd == nd->parent->left) {
+          nd = nd->parent;
+          rr(nd);
         }
-        x->parrent->color = BLACK;
-        x->parrent->parrent->color = RED;
-        lr(x->parrent->parrent);
+        nd->parent->color = BLACK;
+        nd->parent->parent->color = RED;
+        lr(nd->parent->parent);
       }
     }
   }
   root->color = BLACK;
   return 0;
+}
+
+template <typename T> int bitree<T>::del_balance(node *nd) {
+	 while (nd != root && nd->color == BLACK) {
+        if (nd == nd->parent->left) {
+            node *brother = nd->parent->right;
+            if (brother->color == RED) {
+                brother->color = BLACK;
+                nd->parent->color = RED;
+                lr(nd->parent);
+                brother = nd->parent->right;
+            }
+            if (brother->left->color == BLACK && brother->right->color == BLACK) {
+                brother->color = RED;
+                nd = nd->parent;
+            } else {
+                if (brother->right->color == BLACK) {
+                    brother->left->color = BLACK;
+                    brother->color = RED;
+                    rr(brother);
+                    brother = nd->parent->right;
+                }
+                brother->color = nd->parent->color;
+                nd->parent->color = BLACK;
+                brother->right->color = BLACK;
+                lr(nd->parent);
+                nd = root;
+            }
+        } else {
+            node *brother = nd->parent->left;
+            if (brother->color == RED) {
+                brother->color = BLACK;
+                nd->parent->color = RED;
+                rr(nd->parent);
+                brother = nd->parent->left;
+            }
+            if (brother->right->color == BLACK && brother->left->color == BLACK) {
+                brother->color = RED;
+                nd = nd->parent;
+            } else {
+                if (brother->left->color == BLACK) {
+                    brother->right->color = BLACK;
+                    brother->color = RED;
+                    lr(brother);
+                    brother = nd->parent->left;
+                }
+                brother->color = nd->parent->color;
+                nd->parent->color = BLACK;
+                brother->left->color = BLACK;
+                rr(nd->parent);
+                nd = root;
+            }
+        }
+    }
+    nd->color = BLACK;
+	return 0;
 }
 
 template <typename T> int bitree<T>::rc(node *nd) {
@@ -132,20 +237,20 @@ template <typename T> int bitree<T>::lr(node *nd) {
     return 1;
   pNode->right = cNode->left;
   if (cNode->left != nullptr)
-    cNode->left->parrent = pNode;
+    cNode->left->parent = pNode;
   if (cNode != nullptr)
-    cNode->parrent = pNode->parrent;
-  if (pNode->parrent) {
-    if (pNode == pNode->parrent->left)
-      pNode->parrent->left = cNode;
+    cNode->parent = pNode->parent;
+  if (pNode->parent) {
+    if (pNode == pNode->parent->left)
+      pNode->parent->left = cNode;
     else
-      pNode->parrent->right = cNode;
+      pNode->parent->right = cNode;
   } else {
     root = cNode;
   }
   cNode->left = pNode;
   if (pNode != nullptr)
-    pNode->parrent = cNode;
+    pNode->parent = cNode;
   return 0;
 }
 
@@ -156,19 +261,19 @@ template <typename T> int bitree<T>::rr(node *nd) {
     return 1;
   pNode->left = cNode->right;
   if (cNode->right != nullptr)
-    cNode->right->parrent = pNode;
+    cNode->right->parent = pNode;
   if (cNode != nullptr)
-    cNode->parrent = pNode->parrent;
-  if (pNode->parrent) {
-    if (pNode == pNode->parrent->right)
-      pNode->parrent->right = cNode;
+    cNode->parent = pNode->parent;
+  if (pNode->parent) {
+    if (pNode == pNode->parent->right)
+      pNode->parent->right = cNode;
     else
-      pNode->parrent->left = cNode;
+      pNode->parent->left = cNode;
   } else {
     root = cNode;
   }
   cNode->right = pNode;
   if (pNode != nullptr)
-    pNode->parrent = cNode;
+    pNode->parent = cNode;
   return 0;
 }
--- src/bitree/test.cpp
+++ src/bitree/test.cpp
@@ -1,12 +1,13 @@
 #include "bitree.h"
 
-int main(){
+int main() {
   /*int a[] = {59,5,27,34,45,56,73,91,10,19,31,50,78,85,14,40,65,82,24,69,62};
   bitree<int> test;*/
-  std::string a[] = {"Denis", "Nastya", "Basil", "Larisa", "Olya", "Natasha", "Liza", "Vova", "Sergey", "Sasha"};
+  std::string a[] = {"Denis",   "Nastya", "Basil", "Larisa", "Olya",
+                     "Natasha", "Liza",   "Vova",  "Sergey", "Sasha"};
   std::string b[] = {"13", "8", "15", "27", "25", "6", "22", "1", "11", "17"};
   bitree<std::string> test;
-  test.set(a, 10);
+  test.add(a, 10);
   test.show();
   return 0;
 }
