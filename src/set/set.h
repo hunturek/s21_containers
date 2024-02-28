8d203cc588ff2ea30585d0d42672962f0dce8119
Denis <shahovdenis@bk.ru>
2024-02-28 15:55:10 +0300

    set tests

---
+++ src/set/set.h
@@ -0,0 +1,103 @@
+#ifndef SET_H
+#define SET_H
+#include <iostream>
+#include "../bitree/bitree.h"
+
+namespace s21 {
+
+template <typename Key> class set {
+private:
+  using key_type = Key;
+  using value_type = Key;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using size_type = size_t;
+  bitree<Key, Key> tree;
+
+public:
+  using iterator = typename bitree<Key, Key>::tree_iterator;
+  set() {};
+  set(std::initializer_list<value_type> const& items){
+    for (const auto& item : items) {
+        tree << std::make_pair(item, item);
+    }
+  };
+  set(const set& other) : tree(other.tree) {}
+  set(set&& other) noexcept : tree(std::move(other.tree)) {}
+  ~set() {tree.clear();}
+  set& operator=(set&& other) noexcept {
+    if (this != &other) {
+      tree = std::move(other.tree);
+    }
+    return *this;
+  }
+
+  set& operator=(std::initializer_list<value_type> const& items) {
+    tree.clear();
+    for (const auto& item : items) {
+        tree << std::make_pair(item, item);
+    }
+    return *this;
+  }
+
+  iterator begin() {
+    return tree.begin();
+  }
+  iterator end() {
+    return tree.end();
+  }
+
+  bool empty() {
+    if(tree.get_root() == nullptr)
+      return true;
+    else
+      return false;
+  }
+  size_type size() {
+    return tree.tree_size;
+  }
+  size_type max_size() {
+    return 11111;
+  }
+
+  void clear() {
+    tree.clear();
+  }
+  std::pair<iterator, bool> insert(const value_type& value) {
+    iterator it = tree.begin();
+    if(contains(value))
+      return {it.set(value), false};
+    tree << std::make_pair(value, value);
+    return {it.set(value), true};
+  }
+  void erase(iterator it) {
+    tree >> it.cget();
+  }
+  void merge(set& other) {
+    iterator it = other.begin();
+    for(size_type i = 0; i < other.size(); i++){
+      this->insert(it.cget());
+      ++it;
+    }
+  }
+
+  iterator find(const Key& key) {
+    iterator it = this->end();
+    if(!this->contains(key))
+      return it;
+    return it.set(key);
+  }
+
+  bool contains(const Key& key) {
+  try {
+    tree.find_value(key);
+    return true;
+    } catch (const std::out_of_range& e) {
+      return false;
+    }
+  }
+};
+
+}
+
+#endif
---
+++ src/set/test_s21_set.cc
@@ -0,0 +1,132 @@
+#include <gtest/gtest.h>
+
+#include "set.h"
+
+TEST(s21_set, empty_constructor) {
+  s21::set<int> s;
+  ASSERT_EQ(0, s.size());
+  ASSERT_TRUE(s.empty());
+}
+
+TEST(s21_set, initializer_constructor) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  ASSERT_EQ(14, s.size());
+  ASSERT_FALSE(s.empty());
+}
+
+TEST(s21_set, copy_contructor) {
+  s21::set<int> s1 = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int> s2(s1);
+  ASSERT_EQ(14, s2.size());
+  ASSERT_FALSE(s2.empty());
+}
+
+TEST(s21_set, move_constructor) {
+  s21::set<int> s1 = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int> s2(std::move(s1));
+  ASSERT_EQ(14, s2.size());
+  ASSERT_FALSE(s2.empty());
+}
+
+TEST(s21_set, move_assigment) {
+  s21::set<int> s1 = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int> s2;
+  s2 = std::move(s1);
+  ASSERT_EQ(14, s2.size());
+  ASSERT_FALSE(s2.empty());
+}
+
+TEST(s21_set, iterator) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int>::iterator it = s.begin();
+  ASSERT_EQ(02, it.cget());
+  ++it;
+  ASSERT_EQ(12, it.cget());
+  ++it;
+  ASSERT_EQ(13, it.cget());
+  it.last_node();
+  ASSERT_EQ(93, it.cget());
+  ++it;
+  ASSERT_EQ(02, it.cget());
+  --it;
+  ASSERT_EQ(93, it.cget());
+}
+
+TEST(s21_set, clear) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  ASSERT_EQ(14, s.size());
+  ASSERT_FALSE(s.empty());
+  s.clear();
+  ASSERT_TRUE(s.empty());
+}
+
+TEST(s21_set, insert) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int>::iterator it = s.begin();
+  auto res1 = s.insert(40);
+  ASSERT_EQ(res1, std::make_pair(it.set(40), true));
+  auto res2 = s.insert(41);
+  ASSERT_EQ(res2, std::make_pair(it.set(41), true));
+  auto res3 = s.insert(40);
+  ASSERT_EQ(res3, std::make_pair(it.set(40), false));
+  ASSERT_TRUE(s.contains(40));
+}
+
+TEST(s21_set, erase) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int>::iterator it = s.begin();
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+  ++it;
+  ASSERT_EQ(39, it.cget());
+  ASSERT_TRUE(s.contains(39));
+  s.erase(it);
+  ASSERT_FALSE(s.contains(39));
+}
+
+TEST(s21_set, merge) {
+  s21::set<int> s1 = {
+    54, 93, 23, 58, 12, 02, 29, 67
+  };
+  s21::set<int> s2 = {
+    67, 83, 71, 39, 91, 47, 13
+  };
+  ASSERT_EQ(8, s1.size());
+  ASSERT_EQ(7, s2.size());
+  s1.merge(s2);
+  ASSERT_EQ(14, s1.size());
+}
+
+TEST(s21_set, find) {
+  s21::set<int> s = {
+    54, 93, 23, 58, 12, 02, 29, 67, 83, 71, 39, 91, 47, 13
+  };
+  s21::set<int>::iterator it1 = s.find(67);
+  ASSERT_EQ(it1.cget(), 67);
+  s21::set<int>::iterator it2 = s.find(100);
+  ASSERT_EQ(it2.cget(), 93);
+}
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
